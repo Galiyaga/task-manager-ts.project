46b353b5c10a3c3120e545ef6168ae5747ad4cf3
Galiya <galiyaga@yandex.ru>
2025-08-22 17:55:37 +0300

    loading стили изменены, чтобы не было эффекта дёргающегося экрана

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -4,15 +4,31 @@ import { useSelector } from "react-redux";
 import { AppRootStateType } from "../state/store";
 
 export const Loading = () => {
-
-  const isLoadiing = useSelector(
-    (state: AppRootStateType) => state.loading.isLoading
+  const isLoading = useSelector(
+    (state: AppRootStateType) => state.loading.isLoading,
   );
 
-  if (!isLoadiing) return null;
+  if (!isLoading) return null;
 
   return (
-    <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+    <Box
+      sx={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "transparent", 
+        zIndex: 9999,
+        pointerEvents: "none",
+        opacity: isLoading ? 1 : 0, 
+        transition: "opacity 0.2s ease-in-out",
+        visibility: isLoading ? "visible" : "hidden",
+      }}
+    >
       <CircularProgress />
     </Box>
   );
